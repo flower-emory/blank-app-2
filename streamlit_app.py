c554e9df5a388120c837e9fadb45ec994d2d3c74
flower-emory <emory.blackwood@gmail.com>
2024-12-11 19:37:26 +0000

    fuytfrtyuiugf

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,23 @@ st.write("Your current goal is ", goal)
 
 
 name = st.text_input("What is your name?")
-st.write("Nice too meat you ", name)
+st.write("Nice to meet you ", name)
 
 
-# If you want to edit a LIST!
-name_list = st.data_editor(
-    ["$0.00"], 
+
+money_list = st.data_editor(
+    [0.00], 
     num_rows="dynamic"
 )
 
-st.text(name_list)
+st.text(money_list)
 
-st.balloons()
-st.write("you have reached your goal")
+money=0
+
+for coin in money_list:
+    money=money+coin
+st.write(money)
+
+if goal <= money:
+    st.balloons()
+    st.write("you have reached your goal")
