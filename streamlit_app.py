d421c80f80614a1688ac1a06fe922560c1689fe0
flower-emory <emory.blackwood@gmail.com>
2024-12-13 20:00:13 +0000

    bob

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,40 @@
 import streamlit as st
 from sqlitedict import SqliteDict
 
+from sqlitedict import SqliteDict
+
+def save_data(name, data):
+    with SqliteDict("example.sqlite", autocommit=True) as db:
+        db[name] = data
+
+def get_data(name):
+    with SqliteDict("example.sqlite") as db:
+        try:
+            return db[name]
+        except KeyError:
+            return None
+
 st.title("this is your savings")
-
-goal = st.number_input("What is your goal?")
-st.write("Your current goal is ", goal)
-
 
 name = st.text_input("What is your name?")
 st.write("Nice to meet you ", name)
 
+if name not in st.session_state:
+    data=get_data(name)
+    if not data:
+        data=[0.00]
+    st.session_state[name] = data
+
+#data = get_data(name)
+
+goal = st.number_input("What is your goal?", value=5)
+st.write("Your current goal is ", goal)
 
 
 money_list = st.data_editor(
-    [0.00], 
-    num_rows="dynamic"
+    st.session_state[name], 
+    num_rows="dynamic",
 )
-
-st.text(money_list)
-
 money=0
 
 for coin in money_list:
@@ -28,3 +44,5 @@ st.write(money)
 if goal <= money:
     st.balloons()
     st.write("you have reached your goal")
+
+save_data(name,money_list)
